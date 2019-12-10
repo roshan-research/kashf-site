54d20d69bf762a35547b238d09ce513df7b25693
Amin_Khataei <khataei.amin@gmail.com>
2019-12-10 14:07:52 +0330

    responsive

--- src/js/_navbar.js
+++ src/js/_navbar.js
@@ -1,17 +1,29 @@
-export default function () {
+export default function() {
     const navbar = document.querySelector('#navbar');
     const heroBottom = document.querySelector('#hero').offsetHeight;
     let scrollTop = 0;
 
     function checkState() {
         scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-        if(scrollTop > heroBottom) {
+        if (scrollTop > heroBottom) {
             navbar.classList.add('--condensed');
+            $(".navbar-toggler>.line").css("background-color", "#747474");
+
+
+            if (window.matchMedia('(max-width: 767px)').matches)
+                $("nav#navbar .container").css("background-color", "#fff");
+
         } else {
             navbar.classList.remove('--condensed');
+            $(".navbar-toggler>.line").css("background-color", "white");
+
+            if (window.matchMedia('(max-width: 767px)').matches) {
+                $("nav#navbar .container").css("background-color", "#19293b");
+            }
         }
     }
 
     checkState();
     document.addEventListener('scroll', checkState);
 }
+``
